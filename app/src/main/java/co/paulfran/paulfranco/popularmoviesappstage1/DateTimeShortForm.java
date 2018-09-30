ea151a3ff790a7795a358e2b04ae05c66af87ccf
Paul Franco <paulfranco@me.com>
2018-09-30 10:34:22 -0700

    added orentation vertical only to app

--- app/src/main/java/co/paulfran/paulfranco/popularmoviesappstage1/DateTimeShortForm.java
+++ app/src/main/java/co/paulfran/paulfranco/popularmoviesappstage1/DateTimeShortForm.java
@@ -15,9 +15,7 @@ class DateTimeShortForm {
         return simpleDateFormat.parse(date);
     }
 
-
     // Return date in short form based on the device settings.
-
     public static String getLocalizedDate(Context context, String date, String format)
             throws ParseException {
         DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(context);
